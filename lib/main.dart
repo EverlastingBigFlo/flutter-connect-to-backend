5d91cf351df880728a5f953e03ba0b285316dada
everlastingbigflo <anjorin199@gmail.com>
2024-03-08 09:56:12 +0000

    8 Mar 2024, 10:56

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,81 @@
-import 'package:connectingtobackend/otp.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+class OTPTextField extends StatefulWidget {
+  @override
+  _OTPTextFieldState createState() => _OTPTextFieldState();
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({Key? key}) : super(key: key);
+class _OTPTextFieldState extends State<OTPTextField> {
+  late List<TextEditingController> controllers;
+  late List<FocusNode> focusNodes;
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
+  void initState() {
+    super.initState();
+    controllers = List.generate(4, (index) => TextEditingController());
+    focusNodes = List.generate(4, (index) => FocusNode());
+    for (int i = 0; i < controllers.length - 1; i++) {
+      controllers[i].addListener(() {
+        if (controllers[i].text.isNotEmpty) {
+          FocusScope.of(context).requestFocus(focusNodes[i + 1]);
+        }
+      });
+    }
+  }
 
-class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+    for (var controller in controllers) {
+      controller.dispose();
+    }
+    for (var focusNode in focusNodes) {
+      focusNode.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'form',
-      routes: {
-        'form': (context) => MyForm(),
-      },
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(
+        controllers.length,
+        (index) => SizedBox(
+          width: 50,
+          child: TextField(
+            controller: controllers[index],
+            focusNode: focusNodes[index],
+            textAlign: TextAlign.center,
+            keyboardType: TextInputType.number,
+            maxLength: 1,
+            onChanged: (value) {
+              if (value.isNotEmpty && index < controllers.length - 1) {
+                FocusScope.of(context).requestFocus(focusNodes[index + 1]);
+              }
+            },
+            decoration: InputDecoration(
+              counterText: '',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('OTP TextField Example'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: OTPTextField(),
+        ),
+      ),
+    ),
+  ));
 }
