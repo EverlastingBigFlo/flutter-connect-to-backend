8df6cbda996a32a5fc0c684b924dc33e7a33c06d
everlastingbigflo <anjorin199@gmail.com>
2024-03-08 10:27:31 +0000

    8 Mar 2024, 11:27

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,26 @@
+import 'package:connectingtobackend/otp.dart';
 import 'package:flutter/material.dart';
 
-class OTPTextField extends StatefulWidget {
-  @override
-  _OTPTextFieldState createState() => _OTPTextFieldState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _OTPTextFieldState extends State<OTPTextField> {
-  late List<TextEditingController> controllers;
-  late List<FocusNode> focusNodes;
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  void initState() {
-    super.initState();
-    controllers = List.generate(4, (index) => TextEditingController());
-    focusNodes = List.generate(4, (index) => FocusNode());
-    for (int i = 0; i < controllers.length - 1; i++) {
-      controllers[i].addListener(() {
-        if (controllers[i].text.isNotEmpty) {
-          FocusScope.of(context).requestFocus(focusNodes[i + 1]);
-        }
-      });
-    }
-  }
+  State<MyApp> createState() => _MyAppState();
+}
 
-  @override
-  void dispose() {
-    for (var controller in controllers) {
-      controller.dispose();
-    }
-    for (var focusNode in focusNodes) {
-      focusNode.dispose();
-    }
-    super.dispose();
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(
-        controllers.length,
-        (index) => SizedBox(
-          width: 50,
-          child: TextField(
-            controller: controllers[index],
-            focusNode: focusNodes[index],
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            maxLength: 1,
-            onChanged: (value) {
-              if (value.isNotEmpty && index < controllers.length - 1) {
-                FocusScope.of(context).requestFocus(focusNodes[index + 1]);
-              }
-            },
-            decoration: InputDecoration(
-              counterText: '',
-              border: OutlineInputBorder(),
-            ),
-          ),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'form',
+      routes: {
+        'form': (context) => MyForm(),
+      },
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('OTP TextField Example'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: OTPTextField(),
-        ),
-      ),
-    ),
-  ));
 }
--- lib/otp.dart
+++ lib/otp.dart
@@ -1,4 +1,18 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class MyForm extends StatefulWidget {
+  const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
 // class MyForm extends StatefulWidget {
 //   @override
@@ -75,3 +89,5 @@
 //     );
 //   }
 // }
+
+
