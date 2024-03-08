22136b15fec22d57729163d7549e88f8d341f92d
everlastingbigflo <anjorin199@gmail.com>
2024-03-08 09:41:46 +0000

    8 Mar 2024, 10:41

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,47 @@
 import 'package:connectingtobackend/otp.dart';
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       initialRoute: 'form',
+//       routes: {
+//         'form': (context) => MyForm(),
+//       },
+//     );
+//   }
+// }
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: 'otp',
-      routes: {
-        'otp': (context) => MyForm(),
-      },
+      title: 'Text Field Example',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Text Field Example'),
+        ),
+        body: Center(
+          child: MyForm(),
+        ),
+      ),
     );
   }
 }
