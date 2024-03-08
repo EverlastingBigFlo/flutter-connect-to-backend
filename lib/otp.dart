5ac906a5a7a1e8f62cc542c9accc9654c07e9672
everlastingbigflo <anjorin199@gmail.com>
2024-03-08 10:37:08 +0000

    8 Mar 2024, 11:37

--- lib/otp.dart
+++ lib/otp.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
 
 class MyForm extends StatefulWidget {
-  const MyForm({super.key});
+  const MyForm({Key? key});
 
   @override
   State<MyForm> createState() => _MyFormState();
 }
 
 class _MyFormState extends State<MyForm> {
+  // final TextEditingController _controller1 = TextEditingController();
+  // final TextEditingController _controller2 = TextEditingController();
+  // final TextEditingController _controller3 = TextEditingController();
+  // final TextEditingController _controller4 = TextEditingController();
+
+  @override
+  // void dispose() {
+  //   super.dispose();
+  //   _controller1.dispose();
+  //   _controller2.dispose();
+  //   _controller3.dispose();
+  //   _controller4.dispose();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.amber,
+      child: const Column(
+        children: [TextField()],
+      ),
+    );
   }
 }
+
 
 // class MyForm extends StatefulWidget {
 //   @override
