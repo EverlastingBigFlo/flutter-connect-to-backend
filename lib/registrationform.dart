2c8f9cff79572154af2e3c5c82ee57ada38d237b
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-11 21:34:01 +0000

    11 Mar 2024, 14:34

--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -10,15 +10,15 @@ class MyForm extends StatefulWidget {
 }
 
 class _MyFormState extends State<MyForm> {
-  final TextEditingController _controller1 = TextEditingController();
-  final TextEditingController _controller2 = TextEditingController();
+  final TextEditingController firstname = TextEditingController();
+  final TextEditingController lastname = TextEditingController();
   final TextEditingController _controller3 = TextEditingController();
   final TextEditingController _controller4 = TextEditingController();
   @override
   void dispose() {
     super.dispose();
-    _controller1.dispose();
-    _controller2.dispose();
+    firstname.dispose();
+    lastname.dispose();
     _controller3.dispose();
     _controller4.dispose();
   }
@@ -32,7 +32,7 @@ class _MyFormState extends State<MyForm> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
          TextField(
-              controller: _controller1,
+              controller: firstname,
               decoration: InputDecoration(labelText: 'Username'),
             ),
             SizedBox(height: 12),
