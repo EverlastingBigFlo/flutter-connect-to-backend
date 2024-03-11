37cccd4ccba987e9c39e00f0d9444295a87bfce1
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-11 22:07:27 +0000

    11 Mar 2024, 15:07

--- lib/components/my-text.dart
+++ lib/components/my-text.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MyText extends StatelessWidget {
-  const MyText({super.key});
+
+  final Color color;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final FontWeight fontweight;
+  
+  const MyText({super.key, required this.color, required this.fontSize, required this.fontWeight, required this.fontweight});
 
   @override
   Widget build(BuildContext context) {
