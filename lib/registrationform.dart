2743825db4360fe18cd43f046b4775c744d375c2
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-11 23:13:22 +0000

    11 Mar 2024, 16:13

--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -62,137 +62,143 @@ class _MyFormState extends State<MyForm> {
                 ),
 
                 // textfield area
-                const SizedBox(height: 35),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: firstnameController,
-                    decoration: InputDecoration(
-                      hintText: 'First and Last Name',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
+                Column(
+                  children: [
+                    const SizedBox(height: 35),
+                    Container(
+                      decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: firstnameController,
+                        decoration: InputDecoration(
+                          hintText: 'First and Last Name',
+                          hintStyle: const TextStyle(color: Color(0xFF470037)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
+                    const SizedBox(height: 25),
+                    Container(
+                      decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: const TextStyle(color: Color(0xFF470037)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: phoneController,
-                    decoration: InputDecoration(
-                      hintText: 'Phone Number',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
+                    const SizedBox(height: 25),
+                    Container(
+                      decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: phoneController,
+                        decoration: InputDecoration(
+                          hintText: 'Phone Number',
+                          hintStyle: const TextStyle(color: Color(0xFF470037)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
+                    const SizedBox(height: 25),
+                    Container(
+                      decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: const TextStyle(color: Color(0xFF470037)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 25),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextField(
-                    controller: confirmPasswordController,
-                    decoration: InputDecoration(
-                      hintText: 'Confirm Password',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
+                    const SizedBox(height: 25),
+                    Container(
+                      decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: confirmPasswordController,
+                        decoration: InputDecoration(
+                          hintText: 'Confirm Password',
+                          hintStyle: const TextStyle(color: Color(0xFF470037)),
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                const SizedBox(height: 25),
+                    const SizedBox(height: 25),
+                  ],
+                )
+              
+              
               ],
             ),
           )),
