757a7b0dd372c4a40ef4ca715940c53b9be5b8f4
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-11 15:58:07 +0000

    11 Mar 2024, 08:58

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+    const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: PinInputScreen(),
     initialRoute: '/',
 routes: {
   // '/': (context) => PinInputScreen(),
--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -25,55 +25,11 @@ class _MyFormState extends State<MyForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            controller: _controller1,
-            decoration: InputDecoration(
-              labelText: 'Text Field 1',
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            controller: _controller2,
-            decoration: InputDecoration(
-              labelText: 'Text Field 2',
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            controller: _controller3,
-            decoration: InputDecoration(
-              labelText: 'Text Field 3',
-            ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            controller: _controller4,
-            decoration: InputDecoration(
-              labelText: 'Text Field 4',
-            ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              if (_controller1.text.isNotEmpty &&
-                  _controller2.text.isNotEmpty &&
-                  _controller3.text.isNotEmpty &&
-                  _controller4.text.isNotEmpty) {
-                print('All fields have at least one input');
-              } else {
-                // At least one field is empty
-                print('At least one field is empty');
-              }
-            },
-            child: Text('Submit'),
-          ),
-        ],
-      ),
+        
+      )
     );
   }
 }
