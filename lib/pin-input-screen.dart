0c1068df6ba67c87e4fcf90dcc7ae6aedaf9847e
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-12 14:53:56 +0000

    12 Mar 2024, 07:53

--- lib/pin-input-screen.dart
+++ lib/pin-input-screen.dart
@@ -6,88 +6,31 @@ class PinInputScreen extends StatefulWidget {
 }
 
 class _PinInputScreenState extends State<PinInputScreen> {
-  TextEditingController controller1 = TextEditingController();
-  TextEditingController controller2 = TextEditingController();
-  TextEditingController controller3 = TextEditingController();
-  TextEditingController controller4 = TextEditingController();
+  late FocusNode nextFocusnode;
+
+  @override
+  void initState() {
+    super.initState();
+    nextFocusnode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    nextFocusnode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('PIN Input')),
-      ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              PinField(
-                controller: controller1,
-                onChanged: (String value) {
-                  if (value.length == 1) {
-                    FocusScope.of(context).nextFocus();
-                  }
-                },
-              ),
-              PinField(
-                controller: controller2,
-                onChanged: (String value) {
-                  if (value.length == 1) {
-                    FocusScope.of(context).nextFocus();
-                  }
-                },
-              ),
-              PinField(
-                controller: controller3,
-                onChanged: (String value) {
-                  if (value.length == 1) {
-                    FocusScope.of(context).nextFocus();
-                  }
-                },
-              ),
-              PinField(
-                controller: controller4,
-                onChanged: (String value) {
-                  if (value.length == 1) {
-                    FocusScope.of(context).unfocus();
-                  }
-                },
-              ),
-            ],
+        appBar: AppBar(
+          title: Center(child: Text('PIN Input')),
+        ),
+        body: SizedBox(
+          width: 45,
+          child: TextFormField(
+            maxLength: 1,
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class PinField extends StatelessWidget {
-  final TextEditingController controller;
-  final ValueChanged<String> onChanged;
-
-  const PinField({
-    super.key,
-    required this.controller,
-    required this.onChanged,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: 50,
-      child: TextField(
-        controller: controller,
-        textAlign: TextAlign.center,
-        keyboardType: TextInputType.number,
-        maxLength: 1,
-        decoration: const InputDecoration(
-          counterText: '',
-          hintText: '*',
-        ),
-        onChanged: onChanged,
-      ),
-    );
+        ));
   }
 }
