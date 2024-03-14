f15a827c30ab547309cd06e9d877d29f007fd67b
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-14 11:48:32 +0000

    14 Mar 2024, 04:48

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        // '/': (context) => PinInputScreen(),
-        '/': (context) => MyForm(),
+        '/': (context) => PinInputScreen(),
+        // '/': (context) => MyForm(),
       },
     );
   }
