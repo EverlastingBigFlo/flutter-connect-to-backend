d50cdff000ebed61742744d0f35538811e2100ad
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-19 16:52:42 +0000

    19 Mar 2024, 09:52

--- lib/service/dio.dart
+++ lib/service/dio.dart
@@ -5,14 +5,33 @@ final dio = Dio(); // With default `Options`.
 void configureDio() {
   // Set default configs
   dio.options.baseUrl = "http://10.0.2.2:8000/api/";
-  dio.options.connectTimeout = Duration(seconds: 5);
-  dio.options.receiveTimeout = Duration(seconds: 3);
+  dio.options.connectTimeout = const Duration(seconds: 20);
+  dio.options.receiveTimeout = const Duration(seconds: 20);
+  dio.options.headers['accept'] = 'Application/Json';
 
-  // Or create `Dio` with a `BaseOptions` instance.
-  final options = BaseOptions(
-    baseUrl: 'https://api.pub.dev',
-    connectTimeout: Duration(seconds: 5),
-    receiveTimeout: Duration(seconds: 3),
+
+  dio.interceptors.add(
+    InterceptorsWrapper(
+      onRequest: (RequestOptions options, RequestInterceptorHandler handler) {
+        // Do something before request is sent.
+        // If you want to resolve the request with custom data,
+        // you can resolve a `Response` using `handler.resolve(response)`.
+        // If you want to reject the request with a error message,
+        // you can reject with a `DioException` using `handler.reject(dioError)`.
+        return handler.next(options);
+      },
+      onResponse: (Response response, ResponseInterceptorHandler handler) {
+        // Do something with response data.
+        // If you want to reject the request with a error message,
+        // you can reject a `DioException` object using `handler.reject(dioError)`.
+        return handler.next(response);
+      },
+      onError: (DioException error, ErrorInterceptorHandler handler) {
+        // Do something with response error.
+        // If you want to resolve the request with some custom data,
+        // you can resolve a `Response` object using `handler.resolve(response)`.
+        return handler.next(error);
+      },
+    ),
   );
-  final anotherDio = Dio(options);
 }
