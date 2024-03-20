e3559b29d8a5098ac6b7ba62d22d45d1f35b903d
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-20 09:48:33 +0000

    20 Mar 2024, 02:48

--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -33,14 +33,14 @@ class _MyFormState extends State<MyForm> {
   @override
   void initState() {
     super.initState();
-    widget.firstnameController = TextEditingController();
-    widget.emailController = TextEditingController();
-    widget.emailController = TextEditingController();
-    widget.phoneController = TextEditingController();
-    widget.passwordController = TextEditingController();
-    widget.confirmPasswordController = TextEditingController();
-    widget.dateOfBirthController = TextEditingController();
-    widget.genderController = TextEditingController();
+    widget.firstnameController ??= TextEditingController();
+    widget.emailController ??= TextEditingController();
+    widget.emailController ??= TextEditingController();
+    widget.phoneController ??= TextEditingController();
+    widget.passwordController ??= TextEditingController();
+    widget.confirmPasswordController ??= TextEditingController();
+    widget.dateOfBirthController ??= TextEditingController();
+    widget.genderController ??= TextEditingController();
   }
 
   @override
