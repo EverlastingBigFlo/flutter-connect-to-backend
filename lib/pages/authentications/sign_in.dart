ebbdfed4b856bad9024d27df08cccfa8f0cf02db
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-27 14:16:03 +0100

    changes

--- lib/pages/authentications/sign_in.dart
+++ lib/pages/authentications/sign_in.dart
@@ -1,4 +1,6 @@
+import 'package:connectingtobackend/components/my-text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SignIn extends StatefulWidget {
@@ -9,7 +11,16 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  bool isPasswordObscure = true;
+  bool _isObscured = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _isObscured = !_isObscured;
+    });
+  }
+
+  TextEditingController? userNameController;
+  TextEditingController? passwordController;
   void setLogin() async {
     final SharedPreferences prefer = await SharedPreferences.getInstance();
     prefer.setBool("loggedIn", true);
@@ -20,7 +31,188 @@ class _SignInState extends State<SignIn> {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
-      
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Consumer(builder: (context, ref, _) {
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 35),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // intro section
+                    MyText(
+                        color: Color(0xFF470037),
+                        fontSize: 2.5,
+                        fontWeight: FontWeight.bold,
+                        text: 'Welcome')
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    MyText(
+                        color: Color(0xFF470037),
+                        fontSize: 1,
+                        fontWeight: FontWeight.normal,
+                        text: 'Sign in your credientials'),
+                  ],
+                ),
+
+                const SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.09),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: TextFormField(
+                    controller: userNameController,
+                    decoration: InputDecoration(
+                      hintText: 'Username',
+                      hintStyle: const TextStyle(color: Color(0xFF470037)),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+
+                Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.09),
+                            spreadRadius: 5,
+                            blurRadius: 10,
+                            offset: const Offset(0, 5),
+                          )
+                        ]),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      obscureText: _isObscured,
+                      controller: passwordController,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: const TextStyle(color: Color(0xFF470037)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                        suffixIcon: IconButton(
+                          icon: Icon(_isObscured
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          onPressed: _togglePasswordVisibility,
+                        ),
+                      ),
+                    )),
+                const SizedBox(height: 20.0),
+
+                // date picker section
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.09),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 20.0),
+
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.09),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 12.0),
+                const SizedBox(
+                  height: 10,
+                ),
+                // sign up button
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF600852),
+                    fixedSize: const Size(double.infinity, 60),
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(8), // Set border radius
+                    ),
+                  ),
+                  child: const Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                // already have an account
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 65),
+                  child: Row(
+                    children: [
+                      const MyText(
+                          color: Color(0xFF470037),
+                          fontSize: 1.0,
+                          fontWeight: FontWeight.normal,
+                          text: 'Already have an Account?'),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, 'signin');
+                        },
+                        child: const Text('Sign In',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF470037))),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
