a4d887257fcc8fa9a5d76cffbc903339b2674c7d
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-27 11:57:33 +0000

    27 Mar 2024, 12:57

--- flutter-app-connect/app/Http/Controllers/UserController.php
+++ flutter-app-connect/app/Http/Controllers/UserController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\utilities;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    use utilities;
+
+    public function createTxPin(Request $request)
+    {
+
+        // validate the pin inputs and check the pin not for a simple pin 
+        $validator = Validator::make($request->all(), [
+            'pin' => ['required', 'digits:4', 'numeric', function ($attribute, $value, $fail) {
+
+                if (
+                    preg_match('/(\d)\1{2,}/', $value) || preg_match('/123|234|345|456|567|678|789|098|987|876|765|654|543|432|321|012/', $value)
+                ) {
+                    $fail('The pin is too simple.');
+                }
+            }],
+
+
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => $validator->errors()->first(),
+                'status' => 'error',
+            ]);
+        }
+        // $user = User::find($request->user()->id)->first();
+        // if (!$user) {
+        //     return response()->json(['status' => 'error', "message" => 'User not found']);
+        // }
+        // $user->tx_pin = Hash::make($request->pin);
+        // $user->save();
+        // return response()->json(['status' => 'ok', "message" => 'You have created your PIN successfully']);
+    }
 }
