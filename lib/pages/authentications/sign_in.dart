1a88598d67cb625505addd2697b1bb4566d382af
EverlastingBigFlo <anjorin199@gmail.com>
2024-03-27 13:41:31 +0000

    27 Mar 2024, 14:41

--- lib/pages/authentications/sign_in.dart
+++ lib/pages/authentications/sign_in.dart
@@ -1,4 +1,5 @@
 import 'package:connectingtobackend/components/my-text.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -64,157 +65,124 @@ class _SignInState extends State<SignIn> {
                 ),
 
                 const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                  child: TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      hintStyle: const TextStyle(color: Color(0xFF470037)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                    ),
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-
-                Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.09),
-                            spreadRadius: 5,
-                            blurRadius: 10,
-                            offset: const Offset(0, 5),
-                          )
-                        ]),
-                    child: TextFormField(
-                      keyboardType: TextInputType.number,
-                      obscureText: _isObscured,
-                      controller: passwordController,
-                      decoration: InputDecoration(
-                        hintText: 'Password',
-                        hintStyle: const TextStyle(color: Color(0xFF470037)),
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0)),
-                        suffixIcon: IconButton(
-                          icon: Icon(_isObscured
-                              ? Icons.visibility
-                              : Icons.visibility_off),
-                          onPressed: _togglePasswordVisibility,
-                        ),
-                      ),
-                    )),
-                const SizedBox(height: 20.0),
-
-                // date picker section
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 20.0),
-
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.09),
-                        spreadRadius: 5,
-                        blurRadius: 10,
-                        offset: const Offset(0, 5),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 12.0),
-                const SizedBox(
-                  height: 10,
-                ),
-                // button section
-                SizedBox.expand(
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(double.infinity, 60),
-                            backgroundColor: const Color(0xFF600852),
-                            fixedSize: const Size(double.infinity, 60),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
+                Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.09),
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                              offset: const Offset(0, 5),
                             ),
-                          ),
-                          child: const Text(
-                            'SIGN IN',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
+                          ],
+                        ),
+                        child: TextFormField(
+                          controller: emailController,
+                          decoration: InputDecoration(
+                            hintText: 'Email',
+                            hintStyle:
+                                const TextStyle(color: Color(0xFF470037)),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        // already have an account
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 65),
-                          child: Row(
-                            children: [
-                              const MyText(
-                                  color: Color(0xFF470037),
-                                  fontSize: 1.0,
-                                  fontWeight: FontWeight.normal,
-                                  text: 'Don`t have an Account?'),
-                              const SizedBox(
-                                width: 5,
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.09),
+                                  spreadRadius: 5,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 5),
+                                )
+                              ]),
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            obscureText: _isObscured,
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              hintText: 'Password',
+                              hintStyle:
+                                  const TextStyle(color: Color(0xFF470037)),
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                              suffixIcon: IconButton(
+                                icon: Icon(_isObscured
+                                    ? Icons.visibility
+                                    : Icons.visibility_off),
+                                onPressed: _togglePasswordVisibility,
                               ),
-                              InkWell(
-                                onTap: () {
-                                  Navigator.pushNamed(context, 'signup');
-                                },
-                                child: const Text('Sign Up',
-                                    style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xFF470037))),
-                              )
-                            ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ), // button section
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size(double.infinity, 60),
+                          backgroundColor: const Color(0xFF600852),
+                          fixedSize: const Size(double.infinity, 60),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                        child: const Text(
+                          'SIGN IN',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      // already have an account
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 65),
+                        child: Row(
+                          children: [
+                            const MyText(
+                                color: Color(0xFF470037),
+                                fontSize: 1.0,
+                                fontWeight: FontWeight.normal,
+                                text: 'Don`t have an Account?'),
+                            const SizedBox(
+                              width: 5,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(context, 'signup');
+                              },
+                              child: const Text('Sign Up',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF470037))),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 )
               ],
