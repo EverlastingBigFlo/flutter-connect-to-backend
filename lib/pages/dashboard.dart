5070340776eb18b033d9e927db4259ae825d7f35
EverlastingBigFlo <anjorin199@gmail.com>
2024-04-01 06:26:55 +0000

    1 Apr 2024, 07:26

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -30,7 +30,18 @@ class _DashboardState extends State<Dashboard> {
                   borderRadius: BorderRadius.only(
                       bottomLeft: Radius.circular(30.0),
                       bottomRight: Radius.circular(30.0))),
-              child: Center(child: Text('data')),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Get Verified',
+                        style: TextStyle(fontSize: 25.sp),
+                      )
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
