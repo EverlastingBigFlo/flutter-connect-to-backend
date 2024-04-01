3d6f35903ed1845edbfb15046563b468bdbbbe02
EverlastingBigFlo <anjorin199@gmail.com>
2024-04-01 05:50:54 +0000

    1 Apr 2024, 06:50

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -11,27 +11,7 @@ class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        children: [
-          Container(
-            height: 200,
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                  Colors.blue,
-                  Colors.red,
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
+
     );
   }
 }
