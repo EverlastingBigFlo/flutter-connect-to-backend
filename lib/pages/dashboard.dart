5827ed9764df7d044aef922363725fe62d0f64dc
EverlastingBigFlo <anjorin199@gmail.com>
2024-04-01 07:31:21 +0000

    1 Apr 2024, 08:31

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -19,63 +19,123 @@ class _DashboardState extends State<Dashboard> {
         backgroundColor: Colors.black,
         body: Column(
           children: [
-            Container(
-              height: 30.5,
-              width: 100.w,
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [
-                      Color(0xFF8A8AD5),
-                      Color(0xFF8771C9),
-                    ],
-                  ),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30.0),
-                      bottomRight: Radius.circular(30.0))),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Column(
-                  children: [
-                    SafeArea(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            Device.orientation == Orientation.portrait
+                ? Container(
+                    // Widget for Portrait
+                    height: 30.5,
+                    width: 100.w,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xFF8A8AD5),
+                            Color(0xFF8771C9),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30.0),
+                            bottomRight: Radius.circular(30.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
                         children: [
-                          Row(
-                            children: [
-                              Text('Get Verified',
-                                  style: GoogleFonts.concertOne(
-                                    textStyle: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22.sp,
-                                        color: Colors.grey[400]),
-                                  )),
-                              const Icon(
-                                Icons.verified,
-                              )
-                            ],
-                          ),
-                          InkWell(
-                            onTap: () {},
-                            child: const Icon(
-                              Icons.supervised_user_circle,
-                              size: 28,
+                          SafeArea(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text('Get Verified',
+                                        style: GoogleFonts.concertOne(
+                                          textStyle: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 22.sp,
+                                              color: Colors.grey[400]),
+                                        )),
+                                    const Icon(
+                                      Icons.verified,
+                                    )
+                                  ],
+                                ),
+                                InkWell(
+                                  onTap: () {},
+                                  child: const Icon(
+                                    Icons.supervised_user_circle,
+                                    size: 28,
+                                  ),
+                                )
+                              ],
                             ),
-                          )
+                          ),
+                          const Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                'Virtual Mobile - No Sim - Get Verify',
+                                style: TextStyle(color: Color(0xFF3d3d54)),
+                              ))
                         ],
                       ),
                     ),
-                    const Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          'Virtual Mobile - No Sim - Get Verify',
-                          style: TextStyle(color: Color(0xFF3d3d54)),
-                        ))
-                  ],
-                ),
-              ),
-            )
+                  )
+                : Container(
+                    // Widget for Landscape
+                    height: 30.5,
+                    width: 100.w,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xFF8A8AD5),
+                            Color(0xFF8771C9),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30.0),
+                            bottomRight: Radius.circular(30.0))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Column(
+                        children: [
+                          SafeArea(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Row(
+                                  children: [
+                                    Text('Get Verified',
+                                        style: GoogleFonts.concertOne(
+                                          textStyle: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 22.sp,
+                                              color: Colors.grey[400]),
+                                        )),
+                                    const Icon(
+                                      Icons.verified,
+                                    )
+                                  ],
+                                ),
+                                InkWell(
+                                  onTap: () {},
+                                  child: const Icon(
+                                    Icons.supervised_user_circle,
+                                    size: 28,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Text(
+                                'Virtual Mobile - No Sim - Get Verify',
+                                style: TextStyle(color: Color(0xFF3d3d54)),
+                              ))
+                        ],
+                      ),
+                    ),
+                  ),
           ],
         ),
       );
