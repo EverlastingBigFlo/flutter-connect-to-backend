5edc813537c472819ef1a47ef485fe117f09bdb5
EverlastingBigFlo <anjorin199@gmail.com>
2024-04-01 13:27:54 +0000

    1 Apr 2024, 14:27

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -26,19 +26,23 @@ class _DashboardState extends State<Dashboard> {
                     height: 30.5.h,
                   )
                 : FirstContainerWidget(height: 35.5.h),
-            Row(
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: const CircleAvatar(
-                    backgroundColor: Color(0xFF1d1d1d),
-                    child: Icon(
-                      Icons.arrow_upward_rounded,
-                      color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: const CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Color(0xFF1d1d1d),
+                      child: Icon(
+                        Icons.arrow_upward_rounded,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                )
-              ],
+                  )
+                ],
+              ),
             )
           ],
         ),
