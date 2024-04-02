870e1ffeaecc97b9b4b34c041e71949aa16357ac
EverlastingBigFlo <anjorin199@gmail.com>
2024-04-02 11:26:11 +0000

    2 Apr 2024, 12:26

--- flutter-app-connect/database/migrations/2024_04_02_112406_create_otps.php
+++ flutter-app-connect/database/migrations/2024_04_02_112406_create_otps.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('otps', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->unsignedInteger('user_id');
             $table->timestamps();
         });
     }
